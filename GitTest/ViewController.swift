01765ea92d67a2edc786bb8528f64e83b7d72155
llll79531 <llll79531@llll79531.local>
2017-03-10 16:13:33 +0800

    Hello World

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello")
+        print("Hello 11111")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
